c1dc493d5dc5a8821ac3131f4506d3434b8e2394
Shelbot77 <alessiodeangelis021@gmail.com>
2023-02-13 17:07:35 +0100

    show results on products.index

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Product;
 
+
 class ProductController extends Controller
 {
     /**
@@ -14,7 +15,8 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        $products = Product::all();
+        return view('products.index', compact('products'));
     }
 
     /**
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -12,6 +12,33 @@
     <header class="bg-dark">
         <h1 class="text-warning text-center p-4">Laravel-dc-comics</h1>
     </header>
+
+    <div class="container py-5">
+        <div class="row gy-5">
+            @foreach ($products as $product)
+            <div class="col-3">
+                <div class="card p-3">
+                    <img class="img-fluid" src="{{$product->image}}" alt="">
+                    <h2>{{$product->title}}</h2>
+                    <pre>{{$product->price}}</pre>
+                    <pre>{{$product->series}}</pre>
+                    <pre>{{$product->type}}</pre>
+
+
+                    <p>{{$product->description}}</p>
+
+
+                    
+                </div>
+            </div>
+                
+            @endforeach
+        </div>
+    </div>
+
+    <footer class="bg-dark">
+        <h1 class="text-warning text-center p-4">Laravel-dc-comics</h1>
+    </footer>
     
 </body>
 </html>
