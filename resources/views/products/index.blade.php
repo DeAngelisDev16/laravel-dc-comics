7d1d86a5863b4003f77fe39188efafe70cda15b2
Shelbot77 <alessiodeangelis021@gmail.com>
2023-02-13 15:05:55 +0100

    create products.index commit

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel-dc-comics</title>
+    <!-- Styles -->
+    @vite('resources/js/app.js')
+</head>
+<body>
+    <header class="bg-dark">
+        <h1 class="text-warning text-center p-4">Laravel-dc-comics</h1>
+    </header>
+    
+</body>
+</html>
