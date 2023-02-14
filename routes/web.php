d554e5fa65cb35e6c28464f62a267a8c52582f11
Shelbot77 <alessiodeangelis021@gmail.com>
2023-02-14 17:05:57 +0100

    correct store route commit

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,5 @@ use App\Http\Controllers\ProductController as ProductController;
 
 Route::get('/products', [ProductController::class, 'index'])->name('products.index');
 Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+Route::post('/products/store', [ProductController::class, 'store'])->name('products.store');
 Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show');
-Route::post('/products', [ProductController::class, 'store'])->name('products.store');
