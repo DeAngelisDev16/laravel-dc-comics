8069c6937912ef088127dd7b5647a71f18b47869
Shelbot77 <alessiodeangelis021@gmail.com>
2023-02-15 15:38:45 +0100

    edit form setup commit

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -5,37 +5,41 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <!-- Styles -->
+    @vite('resources/js/app.js')
 </head>
 <body>
+    <h1 class="text-center p-3">Edit your comic book here!</h1>
     <div class="container pt-3">
-        <form method="POST" action="">
+        <form method="POST" action="{{route('products.update', $product->id)}}">
             @csrf
+            @method('PUT')
             <div class="mb-3">
-                <input type="text" placeholder="Add a comic title" class="form-control" id="" name="title">
+                <input type="text" placeholder="Add a comic title" class="form-control" id="" name="title" value="{{$product->title}}">
             </div>
 
             <div class="mb-3">
-                <input type="text" placeholder="Add a comic description" class="form-control" id="" name="description">
+                <input type="text" placeholder="Add a comic description" class="form-control" id="" name="description" value="{{$product->description}}">
             </div>
 
             <div class="mb-3">
-                <input type="text" placeholder="copy a comic image (URL)" class="form-control" id="" name="image">
+                <input type="text" placeholder="copy a comic image (URL)" class="form-control" id="" name="image" value="{{$product->image}}">
             </div>
 
             <div class="mb-3">
-                <input type="number" placeholder="Add a comic price" class="form-control" id="" name="price">
+                <input type="number" placeholder="Add a comic price" class="form-control" id="" name="price" value="{{$product->price}}">
             </div>
 
             <div class="mb-3">
-                <input type="text" placeholder="Add a comic series" class="form-control" id="" name="series" >
+                <input type="text" placeholder="Add a comic series" class="form-control" id="" name="series" value="{{$product->series}}" >
             </div>
 
             <div class="mb-3">
-                <input type="text" placeholder="Add a release date (YY/MM/DD)" class="form-control" id=""  name="release_date">
+                <input type="text" placeholder="Add a release date (YY/MM/DD)" class="form-control" id=""  name="release_date" value="{{$product->release_date}}">
             </div>
 
             <div class="mb-3">
-                <input type="type" placeholder="Add a comic type" class="form-control"  id="" name="type">
+                <input type="type" placeholder="Add a comic type" class="form-control"  id="" name="type" value="{{$product->type}}">
             </div>
               
               
--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,10 @@ use App\Http\Controllers\ProductController as ProductController;
 |
 */
 
-Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+/* Route::get('/products', [ProductController::class, 'index'])->name('products.index');
 Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
 Route::post('/products/store', [ProductController::class, 'store'])->name('products.store');
 Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show');
+Route::get('/products/{id}/edit', [ProductController::class, 'edit'])->name('products.edit'); */
 
-//Route::resource('products', ProductController::class);
+Route::resource('products', ProductController::class);
