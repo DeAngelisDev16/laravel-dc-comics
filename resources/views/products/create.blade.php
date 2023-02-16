7716fc4d989cf0bf0e98695fb61fbdcb8dda3c8f
Shelbot77 <alessiodeangelis021@gmail.com>
2023-02-16 16:31:08 +0100

    set errors display commit

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -17,19 +17,22 @@
         <form method="POST" action="{{route('products.store')}}">
             @csrf
 
+            @if ($errors->any())
             <div class="alert alert-danger">
                 <ul>
+                        
                     @foreach ($errors->all() as $error)
                     <li>
                         {{$error}}
                     </li>
-                        
+                    
                     @endforeach
-
+                    
                     
                 </ul>
-
+                
             </div>
+            @endif
             <div class="mb-3">
                 <input type="text" placeholder="Add a comic title" class="form-control" id="" name="title">
             </div>
